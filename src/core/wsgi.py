0abc3d721191a5f0bb73f36ebd4f35a84e1ee782
Roman Ulanov <gopomsk@yandex.ru>
2025-10-30 09:22:37 +0400

    initialize project

---
+++ src/core/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for core project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_wsgi_application()
